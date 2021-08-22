817baa309e549ea51efaa3de98b3c6d4a042e3be
Youngsung Kim <youngsun@ucar.edu>
2021-08-22 17:25:54 -0400

    path update in res/main.py

--- tests/res/main.py
+++ tests/res/main.py
@@ -1,5 +1,8 @@
+import os
 import numpy as np
 from errand import Errand
+
+here = os.path.dirname(os.path.abspath(__file__))
 
 N1 = 10
 N2 = 20
@@ -8,14 +11,19 @@ a = np.ones((N1, N2))
 b = np.ones((N1, N2))
 c = np.zeros((N1, N2))
 
-#with Errand("order.ord") as erd:
-with Errand("vecadd2d.ord") as erd:
+#engine = "cuda"
+#engine = "hip"
+#engine = "pthread"
+engine = "openacc-c++"
 
-    # call N1 teams of N2 gofers 
-    gofers = erd.gofers(N1, N2)
+#with Errand(os.path.join(here, "vecadd2d.ord")) as erd:
+with Errand(os.path.join(here, "vecadd2d.ord"), engine=engine) as erd:
 
     # build workshop with input and output, where actual work takes place
     workshop = erd.workshop(a, b, "->", c)
+
+    # call N1 teams of N2 gofers 
+    gofers = erd.gofers(N1, N2)
 
     # let gofers do their work
     gofers.run(workshop)
